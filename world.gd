0d0e697de2e4d7ad269eac6afe304384342c0872
hankedan000 <dany32412@gmail.com>
2025-06-03 09:09:35 -0400

    Update winoows export. Added some @onready usages. Update arcade submodule.

--- world.gd
+++ world.gd
@@ -22,17 +22,28 @@ var curr_round: int = 1
 var p1_info:= PlayerInfo.new()
 var p2_info:= PlayerInfo.new()
 
+@onready var player1:= $player
+@onready var player2:= $player2
+@onready var game_summary1:= $GameSummary
+@onready var game_summary2:= $GameSummary2
+@onready var p1_how_to:= $p1HowTo
+@onready var p2_how_to:= $p2HowTo
+@onready var p1_label:= $p1Label
+@onready var p2_label:= $p2Label
+@onready var p1_menu:= $p1Menu
 @onready var p1_panel: PlayerPanel = $p1Panel
 @onready var p2_panel: PlayerPanel = $p2Panel
+@onready var count_down_timer:= $count_down
+@onready var replay_timer:= $replay
 
 func set_labels(text: String) -> void:
 	set_label_visible(true)
-	$p1Label.set_text(text)
-	$p2Label.set_text(text)
+	p1_label.set_text(text)
+	p2_label.set_text(text)
 			
 func set_label_visible(new_visible: bool) -> void:
-	$p1Label.visible = new_visible
-	$p2Label.visible = new_visible
+	p1_label.visible = new_visible
+	p2_label.visible = new_visible
 	
 func _input(_event) -> void:
 	var p1Actions = ["ui_up","ui_down","ui_left","ui_right","ui_accept"]
@@ -55,13 +66,13 @@ func _input(_event) -> void:
 func _process(_delta) -> void:
 	match(gs):
 		GameState.eNewGame:
-			$p1Menu.visible = true
-			$player.visible = false
-			$player2.visible = false
-			$p1Label.visible = false
-			$p2Label.visible = false
-			$GameSummary.visible = false
-			$GameSummary2.visible = false
+			p1_menu.visible = true
+			player1.visible = false
+			player2.visible = false
+			p1_label.visible = false
+			p2_label.visible = false
+			game_summary1.visible = false
+			game_summary2.visible = false
 			curr_round = 1
 			p1_panel.set_num_rounds(0)
 			p2_panel.set_num_rounds(0)
@@ -81,22 +92,22 @@ func _process(_delta) -> void:
 		GameState.eWaitForSpawn:
 			var is_first_round := curr_round == 1
 			# prevent players from changing direction while
-			$player.set_disabled_user_input(true)
-			$player2.set_disabled_user_input(true)
+			player1.set_disabled_user_input(true)
+			player2.set_disabled_user_input(true)
 			
 			if not curr_game_opts.is_two_player:
 				p2_info.is_ready = true
 				
 			# show how to play graphic
-			$p1HowTo.visible = is_first_round and TheArcade.is_arcade_mode() and not p1_info.is_ready
-			$p2HowTo.visible = is_first_round and TheArcade.is_arcade_mode() and not p2_info.is_ready
+			p1_how_to.visible = is_first_round and TheArcade.is_arcade_mode() and not p1_info.is_ready
+			p2_how_to.visible = is_first_round and TheArcade.is_arcade_mode() and not p2_info.is_ready
 			
-			$p1Menu.visible = false
+			p1_menu.visible = false
 			set_labels("Round %d\nPress button to enter play!" % curr_round)
-			$p1Label.visible = not p1_info.is_ready
-			$p2Label.visible = not p2_info.is_ready
-			$player.visible = p1_info.is_ready
-			$player2.visible = p2_info.is_ready
+			p1_label.visible = not p1_info.is_ready
+			p2_label.visible = not p2_info.is_ready
+			player1.visible = p1_info.is_ready
+			player2.visible = p2_info.is_ready
 				
 			if p1_info.is_ready and p2_info.is_ready:
 				gs = GameState.eRoundBegin
@@ -109,21 +120,21 @@ func _process(_delta) -> void:
 				gs = GameState.eGameOver
 			else:
 				gs = GameState.eCountDown
-				$count_down.start(3)
+				count_down_timer.start(3)
 			
 		GameState.eCountDown:
-			set_labels(str(int($count_down.time_left) + 1))
+			set_labels(str(int(count_down_timer.time_left) + 1))
 			
 		GameState.ePlay:
 			set_label_visible(false)
 			
 		GameState.eRoundOver:
 			# stop both player from moving
-			$player.set_stopped(true)
-			$player2.set_stopped(true)
+			player1.set_stopped(true)
+			player2.set_stopped(true)
 			
 			# star the timer to begin replay
-			$replay.start()
+			$replay_timer.start()
 			
 			# TODO figure out who won the round
 			var whoWon = 1
@@ -141,10 +152,10 @@ func _process(_delta) -> void:
 			pass
 			
 		GameState.eGameOver:
-			$GameSummary.set_scores(p1_info.score,p2_info.score)
-			$GameSummary2.set_scores(p1_info.score,p2_info.score)
-			$GameSummary.visible = true
-			$GameSummary2.visible = true
+			game_summary1.set_scores(p1_info.score,p2_info.score)
+			game_summary2.set_scores(p1_info.score,p2_info.score)
+			game_summary1.visible = true
+			game_summary2.visible = true
 			
 			if p1_info.is_ready or p2_info.is_ready:
 				gs = GameState.eNewGame
@@ -154,12 +165,12 @@ func _on_replay_timeout():
 	gs = GameState.eRoundBegin
 
 func _on_count_down_timeout():
-	$player.set_stopped(false)
-	$player2.set_stopped(false)
+	player1.set_stopped(false)
+	player2.set_stopped(false)
 	
 	# allow players to control again
-	$player.set_disabled_user_input(false)
-	$player2.set_disabled_user_input(not curr_game_opts.is_two_player)
+	player1.set_disabled_user_input(false)
+	player2.set_disabled_user_input(not curr_game_opts.is_two_player)
 	
 	gs = GameState.ePlay
 
